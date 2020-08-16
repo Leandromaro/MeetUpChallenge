a8c0eeadcf376869e257df5f8be16ccac989a429
Leandro Abel Maro <leandromaro@gmail.com>
2020-08-16 08:42:01 -0300

    Adding Attendance default false value

--- src/main/java/com/leandromaro/santander/rest/persistence/domain/MeetUpUsers.java
+++ src/main/java/com/leandromaro/santander/rest/persistence/domain/MeetUpUsers.java
@@ -6,13 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -34,5 +28,6 @@ public class MeetUpUsers {
     @JoinColumn(name = "user_id")
     private UserMeetUp user;
 
-    private Boolean userAttend;
+    @Column(columnDefinition = "boolean default false")
+    private boolean userAttend;
 }
